8c2b80cb4c14870b3f74336fa034c15af8c81a9c
Ryan Tanay <ryantanay@newevolution.org>
2016-07-12 13:32:01 -0500

    stubbed out Flying and Swimming classes

--- zoolandia.py
+++ zoolandia.py
@@ -17,3 +17,15 @@ class Walking:
     def __init__(self):
         self.walk_speed = 0
         self.legs = 0
+
+class Flying:
+    def __init__(self):
+        self.wings = 0
+        self.wing_span = 0
+        self.air_speed = 0
+
+class Swimming:
+    def __init__(self):
+        self.fins = False
+        self.flippers = False
+        self.web_feet = False
