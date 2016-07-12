c0afc0adf0bae386b1a2c8862a556db8f85360b1
Ryan Tanay <ryantanay@newevolution.org>
2016-07-12 10:41:43 -0500

    Started basic testing structure

---
+++ Test_Zoolandia.py
@@ -0,0 +1,12 @@
+import unittest
+import zoolandia
+
+class TestAnimal(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        pass
+
+#class TestSpecies(unittest.TestCase):
+
+#class TestHabitat(unittest.TestCase):
